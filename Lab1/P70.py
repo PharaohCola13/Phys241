bea5bb0a7cbf8aad9f34d6383f93d7b070cae717
Spencer Riley <42317614+PharaohCola13@users.noreply.github.com>
2018-09-05 15:41:43 -0600

    Update P70.py

--- Lab1/P70.py
+++ Lab1/P70.py
@@ -2,7 +2,7 @@
 
 from __future__ import division, print_function
 from visual import *
-from screenshot import GetScreenShot # Required to have screenshot.py
+#from screenshot import GetScreenShot # Required to have screenshot.py
 
 # Create an x,y,z axis with cylinders of different colors
 
