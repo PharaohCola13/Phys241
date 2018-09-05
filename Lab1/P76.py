ce88624049fa6d843b81d6a79d1a2174eee6d7bf
Spencer Riley <42317614+PharaohCola13@users.noreply.github.com>
2018-09-05 15:43:18 -0600

    Update P76.py

--- Lab1/P76.py
+++ Lab1/P76.py
@@ -2,7 +2,7 @@
 
 from __future__ import division, print_function
 from visual import *
-from screenshot import GetScreenShot # Required to have screenshot.py
+#from screenshot import GetScreenShot # Required to have screenshot.py
 
 theta = 0 # Initial definition of theta
 
