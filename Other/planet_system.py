ca0b18050e04e7e4a200a30ed0f9e9c9ed0e66e5
PharaohCola13 <spencerriley620@gmail.com>
2018-09-13 22:32:26 -0600

    update

--- Other/planet_system.py
+++ Other/planet_system.py
@@ -1,15 +1,25 @@
 from __future__ import division, print_function
 from visual import *
 
-star        = sphere()
-star.pos    = vector(0,0,0)
-star.radius = 100
-star.color  = color.orange
+star_out            = sphere()
+star_out.pos        = vector(0,0,0)
+star_out.radius     = 56378.15
+star_out.m          = 1.989E30
+star_out.color      = color.orange
+star_out.opacity    = 0.8
+
+star_in             = sphere()
+star_in.pos         = vector(0,0,0)
+star_in.radius      = 0.75 * 56378.15
+star_in.m           = 0
+star_in.color       = color.yellow
+star_in.opacity     = 1
 
 planet_one          = sphere(make_trail=True)
 planet_one.pos      = vector(15, 15, 0)
-planet_one.radius   = 1
+planet_one.radius   = 200 * 6.378
 planet_one.color    = color.cyan
+planet_one.m        = 5.972E24
 
 theta = 0
 
@@ -23,6 +33,6 @@ while True:
     ij_hat.y = sin(theta)
     ij_hat.z = 0
 
-    planet_one.pos = 3 * star.radius * ij_hat
+    planet_one.pos = 3 * star_out.radius * ij_hat
 
     theta = theta + pi/1000
