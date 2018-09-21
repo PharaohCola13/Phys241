8cf0bdda190e79d36cdc428617217c1cf8e9a28a
Spencer Riley <spencerriley620@gmail.com>
2018-09-21 16:40:54 -0600

    Update planet_system.py

--- Other/planet_system.py
+++ Other/planet_system.py
@@ -9,7 +9,9 @@ def canvas(scene):  # return canvas bounding box, excluding frames
 G       = 6.7e-11 # Nm^2/kg^2
 YinS    = 365.*24.*60.*60 # s
 AU      = 1.5e11 # m
-
+def color():
+    spaghetti = vector(1,0.267, 0)
+    return spfhetti
 # star_out            = sphere()
 # star_out.pos        = vector(0,0,0)
 # star_out.radius     = 56378.15
@@ -40,14 +42,16 @@ star_in             = sphere()
 star_in.pos         = vector(0,0,0)
 star_in.radius      = 0.1
 star_in.m           = 2e30
-star_in.color       = color.yellow
+star_in.color       = spaghetti
 star_in.opacity     = 1
+star_in.materials   = materials.emissive 
 
 planet_one          = sphere(make_trail=True)
 planet_one.pos      = vector(1, 0, 0)
 planet_one.radius   = 0.05
 planet_one.color    = color.cyan
 planet_one.m        = 6e24
+planet_on.materials = materials.BlueMarble
 
 planet_one_ring             = ring()
 planet_one_ring.pos         = planet_one.pos
@@ -61,6 +65,7 @@ planet_two.pos      = vector(0.5, 0.6, 0)
 planet_two.radius   = 0.1
 planet_two.color    = color.white
 planet_two.m        = 1e15
+planet_two.materials = materials.blazed
 
 star_in.v0          = vector(0,0,0)
 planet_one.v0       = vector(0, (2 * pi * AU)/YinS, 0)
@@ -81,27 +86,27 @@ dt = YinS/1000
 while (planet_two.pos - star_in.pos) >= 1 :
     rate(100)
 
-    r_one = (planet_one.pos - star_in.pos) * AU
-    r_one_mag  = mag(r_one)
-    r_one_hat = norm(r_one)
+#     r_one = (planet_one.pos - star_in.pos) * AU
+#     r_one_mag  = mag(r_one)
+#     r_one_hat = norm(r_one)
 
     r_two = (planet_two.pos - star_in.pos)*AU
     r_two_mag = mag(r_two)
     r_two_hat = norm(r_two)
 
-    F_one_mag = G*(star_in.m * planet_one.m)/r_one_mag**2
-    F_one_net = -F_one_mag * r_one_hat
+#     F_one_mag = G*(star_in.m * planet_one.m)/r_one_mag**2
+#     F_one_net = -F_one_mag * r_one_hat
 
     F_two_mag = G*(star_in.m * planet_two.m)/r_two_mag**2
     F_two_net = -F_two_mag * r_two_hat
 
-    planet_one.p = planet_one.p + F_one_net * dt
-    planet_one_ring.p = planet_one.p
+#     planet_one.p = planet_one.p + F_one_net * dt
+#     planet_one_ring.p = planet_one.p
 
     planet_two.p = planet_two.p + F_two_net * dt
 
-    planet_one.pos = planet_one.pos + (planet_one.p/planet_one.m/AU) * dt
-    planet_one_ring.pos = planet_one.pos
+#     planet_one.pos = planet_one.pos + (planet_one.p/planet_one.m/AU) * dt
+#     planet_one_ring.pos = planet_one.pos
 
     #planet_two.pos = planet_two.pos + (planet_two.p/planet_two.m/AU) * dt
     planet_two.m = planet_two.m + 1e20
@@ -117,7 +122,14 @@ while (planet_two.pos - star_in.pos) >= 1 :
     F_sun_net = -F_sun_mag * r_sun_hat
 
     star_in.p = star_in.p + F_sun_net * dt
-
+    
+    if r_two_mag < star_in.radius:
+        star_in.color = color.black
+        planet_two.color = color.black
+        display(center=star_in.pos,background=(1,1,1))
+        break
+        
+    
     star_in.pos = star_in.pos + (star_in.p/star_in.m/AU) * dt
     #print(planet_two.m)
     t= t + dt
