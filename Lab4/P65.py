1586010f4446515b364692f12930662c81d09a6b
Spencer Riley <spencerriley620@gmail.com>
2018-09-21 16:21:21 -0600

    Update P65.py

--- Lab4/P65.py
+++ Lab4/P65.py
@@ -1,17 +1,18 @@
 from __future__ import division, print_function
 from visual import *
 
-mcraft = 1.5e4 #kg
-dt = 60 # s
+mcraft = 1.5e4 # mass of craft in kg
+dt = 60 # in s
 t = 0
-G = 6.7e-11
+G = 6.7e-11 # Gravitational Constant
 
+# Creates a sphere representing Earth
 Earth = sphere()
 Earth.pos = vector(0,0,0)
 Earth.radius = 1
 Earth.m = 1
 
-
+# Creates a craft
 craft = sphere(make_trail=True)
 craft.v = vector(0,2e3,0) # m/s
 craft.m = mcraft
@@ -21,14 +22,18 @@ craft.p = craft.m * craft.v
 
 while True:
     rate(1)
+    # Distance
     r = craft.pos - Earth.pos
     rmag = mag(r)
     rhat = norm(r)
-
+    
+    # Force of gravity
     fgrav = G * (craft.m * Earth.m)/(rmag**2)
     fnet  = -fgrav * rhat
-
+    
+    # Updates craft's momentum
     craft.p = craft.p + fnet * dt
+    # Updates craft's position
     craft.pos = craft.pos + (craft.p/craft.m) * dt
 
     t = t + dt
