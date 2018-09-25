fdc698787036f9f78d3c912daa75db698716bb19
Spencer Riley <spencerriley620@gmail.com>
2018-09-25 08:29:25 -0600

    Update P65.py

--- Lab4/P65.py
+++ Lab4/P65.py
@@ -3,10 +3,10 @@ from __future__ import print_function, division
 from visual import *
 
 # Declare constants
-AU2m    = 6.4e7 #meters in an AU
+D       = 6.4e7 # 10 times the radius of the Earth
 G       = 6.7e-11  # Nm2/kg2
-ScaleP = 1e-8
-ScaleF = 1e-4
+ScaleP = 1e-8 # Scalar factor for the momentum arrow
+ScaleF = 1e-4 # Scalar factor for the force arrow
 
 # Create objects
 earth           = sphere()
@@ -14,9 +14,9 @@ earth.pos       = vector(0, 0, 0)
 earth.radius    = 0.1
 earth.material  = materials.earth
 
-earth.v0        = vector(0, 0, 0)
+earth.v        = vector(0, 0, 0)
 earth.m         = 6e24  # kg
-earth.p         = earth.m * earth.v0
+earth.p         = earth.m * earth.v
 
 craft           = sphere(make_trail=True)
 craft.pos       = vector(1, 0, 0)
@@ -24,37 +24,42 @@ craft.radius    = 0.01
 craft.color     = color.white
 
 craft.m         = 1.5e4 # kg
-# Circular Orbit
-craft.v0        = vector(0, sqrt(G * earth.m/(mag(craft.pos) * AU2m)), 0)
 
+# Circular Orbit
+craft.v        = vector(0, sqrt(G * earth.m/(mag(craft.pos) * D)), 0)
 
 # Elliptical Orbit
 #craft.v0        = 0.75 * vector(0, sqrt(G * earth.m/(mag(craft.pos) * AU2m)), 0)
 
+# Arrow used to represent the momentum 
 # momentum = arrow()
 # momentum.color = color.green
 #
+# Arrow used to represent the net force
 # force = arrow()
 # force.color = color.magenta
-# a)
-#craft.v0        = vector(0, sqrt(G * earth.m/(mag(craft.pos) * AU2m)), 0)
-craft.p         = craft.m * craft.v0
 
-t   = 0
-dt  = 60
+craft.p         = craft.m * craft.v
+
+t   = 0 # Initial Time
+dt  = 60 # Time step
 
 while True:
     rate(1e3)
-
-    r           = (craft.pos - earth.pos) * AU2m
+    
+    # Distance between the craft and the earth
+    r           = (craft.pos - earth.pos) * D
     rmag        = mag(r)
     rhat        = norm(r)
-
+    
+    # Gravitational Force
     Fmag        = G * (earth.m * craft.m)/rmag**2
     Fnet        = -Fmag * rhat
 
+    # Update Momentum of craft
     craft.p     = craft.p + Fnet * dt
-    craft.pos   = craft.pos + (craft.p /craft.m/AU2m) * dt
+    # Update Position of craft
+    craft.pos   = craft.pos + (craft.p /craft.m/D) * dt
 
     # momentum.pos = craft.pos
     # momentum.axis = ScaleP * craft.p
