d886925406c847cc42f098c03730f0402728201c
Spencer Riley <spencerriley620@gmail.com>
2018-10-16 10:34:42 -0600

    Update P75.py

--- Lab6/P75.py
+++ Lab6/P75.py
@@ -5,15 +5,17 @@ import warnings
 warnings.simplefilter("error", RuntimeWarning)
 from visual.graph import *
 
-        # Creates the graphical display for position
+# Creates the graphical display for position
 gd1 = gdisplay(x=550,y=0,width=700,height=500,
         title='Not a graph of ball motion',
         xtitle='Time [s]',
         ytitle='Position [m]')
-
-L0  = 0.1 #m
-s0  = 0.12
-k = 2e9
+# Relaxed Length
+L0  = 0.1 # in m
+# Intial Length
+s0  = 0.12 # in m
+# Spring Constant 
+k = 2e9 # in Nm
 
 base                = box()
 base.size           = vector(1, 0.01, 1)
@@ -23,8 +25,8 @@ base.color          = color.green
 ball                = sphere(make_trail=True)
 ball.pos            = vector(s0, 0, 0)
 ball.radius         = 0.025
-ball.m              = 0.1
-ball.v              = vector(-1, 0, 0)
+ball.m              = 0.1 # in kg
+ball.v              = vector(-1, 0, 0) # in m/s
 ball.p              = ball.m * ball.v
 ball.color          = color.cyan
 
@@ -41,7 +43,9 @@ spring.radius       = 0.01
 spring.coils        = 10
 spring.color        = color.orange
 
+# Initial Time
 t = 0
+# Time Step
 dt = 0.01
 
 f1 = gcurve(gdisplay = gd1, color=color.white) # Define functions to plot
@@ -49,22 +53,28 @@ f1 = gcurve(gdisplay = gd1, color=color.white) # Define functions to plot
 while t < 2000 * dt:
     try:
         rate(100)
+# Spring Force
         s           = mag(ball.pos) - L0
         Fspring     = -(k * s**7) * norm(ball.pos)
         Fnet        = Fspring
-
+# Updates Ball Momentum
         ball.p      = ball.p + Fnet * dt
+# Updates Ball Velocity
         ball.v      = (ball.p)/(ball.m)
+# Updates Ball Position
         ball.pos    = ball.pos + ball.v * dt
 
         spring.axis = ball.pos - rod.pos
+# Plots Ball Position as a function of time
         f1.plot(pos=(t, ball.pos.x))
+# Plots Ball Velocity as a function of time
 #        f1.plot(pos=(t, ball.v.x))  # Plots position vs time
+# Plots Strench as a function of time
  #       f1.plot(pos=(t, s), color=color.magenta)
-        
+# Updates Time
         t = t + dt
         print(t)
-
+# Python did no-no stuff, these stopped the script from running the bad errors came.
     except OverflowError:
             print("Over Nope")
             break
