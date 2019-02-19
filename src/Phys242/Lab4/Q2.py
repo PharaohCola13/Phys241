67c657a7d9c28550a069a7b5607db1f931a5ab69
Spencer Riley <spencer.riley@student.nmt.edu>
2019-02-19 10:15:12 -0700

    Update Q2.py

--- src/Phys242/Lab4/Q2.py
+++ src/Phys242/Lab4/Q2.py
@@ -8,16 +8,12 @@ dx = array([1, 0.5, 0.1, 0.01])
 
 print(unicodedata.lookup("GREEK CAPITAL LETTER DELTA")+ "x" + "\t\t" + "Results" + "\t\t" + "Error")
 for i in dx:
-    #print("i = {}".format(i))
     x = arange(xi, xf + i, i)
-
     y = x**4 - 2*x + 1
 
     # Integration
-
     Int = 0
     for j in range(len(x)):
-#        print(len(x))
         try:
             Int = Int + i/3 * (y[j] + 4*y[j+1] + y[j+2])
         except IndexError:
